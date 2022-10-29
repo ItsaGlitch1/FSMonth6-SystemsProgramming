afe70ab8736abacaf67516a77f6996ceda9fe88e
ItsaGlitch1 <jharneyiiii@gmail.com>
2022-10-29 17:15:59 -0400

    lab2

---
+++ Lab2/Lab2/Lab2.cpp
@@ -0,0 +1,35 @@
+// Lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+unsigned int bitField = 19;
+int main()
+{
+    int bit;
+    TurnOn(bit);
+}
+void TurnOn(int bit)
+{
+    
+}
+void TurnOff(int bit)
+{
+
+}
+void Toggle(int bit)
+{
+
+}
+void Negate()
+{
+
+}
+void LeftShift()
+{
+
+}
+void RightShift()
+{
+
+}
