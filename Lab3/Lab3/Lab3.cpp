40d252284a51dcbb10454d28019c1e0107daf441
ItsaGlitch1 <jharneyiiii@gmail.com>
2022-11-03 16:08:46 -0400

    lab 3 done

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -2,7 +2,7 @@
 //
 
 #include <iostream>
-enum EnumColorDefinition {White = 1, Black, Silver, Red, Blue};
+enum EnumColorDefinition {White = 1, Black, Silver, Red, Blue = 5};
 struct Car
 {
 	char Make[32];
@@ -15,23 +15,31 @@ struct Car
 void Program1();
 void Program2_1(int value, int* pointer);
 void Program2_2();
-void Program3_1();
+void Program3();
+void repaintCar(Car* ptrCar, EnumColorDefinition newcolor);
+void printCar(Car c);
+void printCarPointer(Car* ptrCar);
 int main()
 {
 	srand(time(NULL));
+	//Program 1
 	std::cout << "Program 1" << std::endl;
 	Program1();
 
-	std::cout << "Program 2.1" << std::endl;
+	//Program 2.1
+	std::cout << "Program 2" << std::endl;
 	int P2 = 9;
 	int* P2Point = &P2;
 	Program2_1(P2, P2Point);
 
-	std::cout << "Program 2.2" << std::endl;
+	//Program 2.2
 	Program2_2();
+	
+	//Program 3
+	std::cout << "Program 3" << std::endl;
+	Program3();
 
-	std::cout << "Program 3.1" << std::endl;
-	Program3_1();
+	
 
 }
 void Program1()
@@ -57,9 +65,11 @@ void Program2_2()
 		Program2_1(numbers[i], Arraypoint);
 	}
 }
-void Program3_1()
+void Program3()
 {
-	Car Cars[3];
+	Car Cars[3]{};
+	int color = 0;
+	int input;
 	for (int i = 0; i < 3; i++)
 	{
 		std::cout << "What is the make for Car " << i + 1 << '?' << std::endl;
@@ -84,41 +94,125 @@ void Program3_1()
 		}
 		std::cin.ignore(INT_MAX, '\n');
 
-		int color = 0;
 		std::cout << "What is the color for Car " << i + 1 << '?' << " (1. White, 2. Black, 3. Silver, 4. Red, 5. Blue)" << std::endl;
 		std::cin >> color;
 		if (std::cin.fail())
 		{
 			std::cin.clear();
-			color = 1;
 		}
 		std::cin.ignore(INT_MAX, '\n');
+		if (color > 5 || color < 1)
+		{
+			color = 1;
+		}
 		Cars[i].Color = (EnumColorDefinition)color;
 	}
+	std::cout << "Printing Cars..." << std::endl;
 	for (int i = 0; i < 3; i++)
 	{
-		std::cout << "Here are the cars you made:" << std::endl;
-		switch (Cars[i].Color)
+		std::cout << "Car " << (i + 1) << ": ";
+		printCar(Cars[i]);
+	}
+	std::cout << "Printing Car Pointers..." << std::endl;
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << "Car " << (i + 1) << ": ";
+		printCarPointer(&Cars[i]);
+	}
+
+
+	std::cout << "Do you want to repaint a car? (1. Yes, 2. No)" << std::endl;
+	std::cin >> input;
+	if (std::cin.fail())
+	{
+		std::cin.clear();
+	}
+	std::cin.ignore(INT_MAX, '\n');
+	if (input == 1)
+	{
+		std::cout << "which car do you want to repaint? (enter a number)" << std::endl;
+		std::cin >> input;
+		if (std::cin.fail())
 		{
-		case 1:
-			std::cout << "Car " << (i + 1) << ": " << "White " << Cars[i].Year << ' ' << Cars[i].Make << ' ' << Cars[i].Model << " with " << Cars[i].Mileage << " miles." << std::endl;
-			break;
-		case 2:
-			std::cout << "Car " << (i + 1) << ": " << "Black " << Cars[i].Year << ' ' << Cars[i].Make << ' ' << Cars[i].Model << " with " << Cars[i].Mileage << " miles." << std::endl;
-			break;
-		case 3:
-			std::cout << "Car " << (i + 1) << ": " << "Silver " << Cars[i].Year << ' ' << Cars[i].Make << ' ' << Cars[i].Model << " with " << Cars[i].Mileage << " miles." << std::endl;
-			break;
-		case 4:
-			std::cout << "Car " << (i + 1) << ": " << "Red " << Cars[i].Year << ' ' << Cars[i].Make << ' ' << Cars[i].Model << " with " << Cars[i].Mileage << " miles." << std::endl;
-			break;
-		case 5:
-			std::cout << "Car " << (i + 1) << ": " << "White " << Cars[i].Year << ' ' << Cars[i].Make << ' ' << Cars[i].Model << " with " << Cars[i].Mileage << " miles." << std::endl;
-			break;
+			std::cin.clear();
 		}
-
-
-
-
+		std::cout << "What do you want to paint it? (1. White, 2. Black, 3. Silver, 4. Red, 5. Blue)" << std::endl;
+		std::cin >> color;
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+		}
+		std::cin.ignore(INT_MAX, '\n');
+		if (color > 5 || color < 1)
+		{
+			color = 1;
+		}
+		if (input >= 1 && input <= 3)
+			repaintCar(&Cars[input - 1], (EnumColorDefinition)color);
+		
+	}
+}
+void repaintCar(Car* ptrCar, EnumColorDefinition newcolor)
+{
+	ptrCar->Color = newcolor;
+	switch (newcolor)
+	{
+	case 1:
+		std::cout << "White " << ptrCar->Year << ' ' << ptrCar->Make << ' ' << ptrCar->Model << " with " << ptrCar->Mileage << " miles." << std::endl;
+		break;
+	case 2:
+		std::cout << "Black " << ptrCar->Year << ' ' << ptrCar->Make << ' ' << ptrCar->Model << " with " << ptrCar->Mileage << " miles." << std::endl;
+		break;
+	case 3:
+		std::cout << "Silver " << ptrCar->Year << ' ' << ptrCar->Make << ' ' << ptrCar->Model << " with " << ptrCar->Mileage << " miles." << std::endl;
+		break;
+	case 4:
+		std::cout << "Red " << ptrCar->Year << ' ' << ptrCar->Make << ' ' << ptrCar->Model << " with " << ptrCar->Mileage << " miles." << std::endl;
+		break;
+	case 5:
+		std::cout << "Blue " << ptrCar->Year << ' ' << ptrCar->Make << ' ' << ptrCar->Model << " with " << ptrCar->Mileage << " miles." << std::endl;
+		break;
+	}
+}
+void printCar(Car c)
+{
+	switch (c.Color)
+	{
+	case 1:
+		std::cout << "White " << c.Year << ' ' << c.Make << ' ' << c.Model << " with " << c.Mileage << " miles." << std::endl;
+		break;
+	case 2:
+		std::cout << "Black " << c.Year << ' ' << c.Make << ' ' << c.Model << " with " << c.Mileage << " miles." << std::endl;
+		break;
+	case 3:
+		std::cout << "Silver " << c.Year << ' ' << c.Make << ' ' << c.Model << " with " << c.Mileage << " miles." << std::endl;
+		break;
+	case 4:
+		std::cout << "Red " << c.Year << ' ' << c.Make << ' ' << c.Model << " with " << c.Mileage << " miles." << std::endl;
+		break;
+	case 5:
+		std::cout << "Blue " << c.Year << ' ' << c.Make << ' ' << c.Model << " with " << c.Mileage << " miles." << std::endl;
+		break;
+	}
+}
+void printCarPointer(Car* ptrCar)
+{
+	switch (ptrCar->Color)
+	{
+	case 1:
+		std::cout << "White " << ptrCar->Year << ' ' << ptrCar->Make << ' ' << ptrCar->Model << " with " << ptrCar->Mileage << " miles." << std::endl;
+		break;
+	case 2:
+		std::cout << "Black " << ptrCar->Year << ' ' << ptrCar->Make << ' ' << ptrCar->Model << " with " << ptrCar->Mileage << " miles." << std::endl;
+		break;
+	case 3:
+		std::cout << "Silver " << ptrCar->Year << ' ' << ptrCar->Make << ' ' << ptrCar->Model << " with " << ptrCar->Mileage << " miles." << std::endl;
+		break;
+	case 4:
+		std::cout << "Red " << ptrCar->Year << ' ' << ptrCar->Make << ' ' << ptrCar->Model << " with " << ptrCar->Mileage << " miles." << std::endl;
+		break;
+	case 5:
+		std::cout << "Blue " << ptrCar->Year << ' ' << ptrCar->Make << ' ' << ptrCar->Model << " with " << ptrCar->Mileage << " miles." << std::endl;
+		break;
 	}
 }
